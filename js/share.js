91410f2e6cd6fe749c4706528d15b224ddfecd00
kkho5555 <62885208+kkho5555@users.noreply.github.com>
2020-05-15 17:32:42 +0900

    modify index
    share 완성

---
+++ js/share.js
@@ -0,0 +1,48 @@
+Kakao.init('f3b1e7ad3119c871d4c13360f05459e2');
+Kakao.Link.createDefaultButton({
+  container: '#kakao-link-btn',
+  objectType: 'feed',
+  content: {
+    title: document.title,
+    description: '내용, 주로 해시태그',
+    imageUrl: document.images[0].src,
+    link: {
+      webUrl: document.location.href,
+      mobileWebUrl: document.location.href,
+    },
+  },
+  social: {
+    likeCount: 286,
+    commentCount: 45,
+    sharedCount: 845,
+  },
+  buttons: [
+    {
+      title: 'Open!',
+      link: {
+        mobileWebUrl: document.location.href,
+        webUrl: document.location.href,
+      },
+    },
+  ],
+});
+
+// 페이스북
+function fb_share() {
+  var linkUrl = window.location.href;
+  window.open(
+    'http://www.facebook.com/sharer.php?u=' + encodeURIComponent(linkUrl)
+  );
+}
+
+// 트위터
+function tw_share() {
+  window.open(
+    'https://twitter.com/intent/tweet' +
+    '?' +
+    '&text=' +
+    encodeURIComponent('닮은꼴 연예인 찾기') + // Title in this html document
+      '&url=' +
+      encodeURIComponent(window.location.href)
+  );
+}
